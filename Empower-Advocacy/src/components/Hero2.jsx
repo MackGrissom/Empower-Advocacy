ecb7bd0865a7656cadc9dc2ee42e8a718e41d374
Mack Grissom <mcintyregrissom@gmail.com>
2023-01-25 15:14:22 -0500

    bg switch on hero2

--- Empower-Advocacy/src/components/Hero2.jsx
+++ Empower-Advocacy/src/components/Hero2.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '/home/mackgrissom/code/MackGrissom/Empower-Advocacy/Empower-Advocacy/src/assets/images/logo.webp'
-import background from '/home/mackgrissom/code/MackGrissom/Empower-Advocacy/Empower-Advocacy/src/assets/images/background.webp'
+import background from '/home/mackgrissom/code/MackGrissom/Empower-Advocacy/Empower-Advocacy/src/assets/images/background9.webp'
 const Hero2 = () => {
   return (
     <section>
-  <div class="px-4 py-2 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 bg-beige ">
+  <div class="px-4 py-10 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 bg-beige ">
     <div class="flex flex-wrap items-center mx-auto max-w-7xl">
       <div class="w-full lg:max-w-lg lg:w-1/2 rounded-xl">
         <div>
@@ -27,7 +27,7 @@ const Hero2 = () => {
           </div>
         {/* <span class="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase"> <span className='text-gn'> Empower Advocacy </span> </span> */}
         <h1 class="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl"> <span className='text-gn'> Empowering </span>  Families To Navigate Special <span className='text-gn'>Education </span></h1>
-        <p class="mb-8 text-base leading-relaxed text-left text-gray-500">Our mission is to empower  you to advocate for your child in the school system. Parents are often left feeling alone at the table during an IEP meeting. We help change that.</p>
+        <p class="mb-8 text-base leading-relaxed text-left text-gray-500">Our mission is to empower parents to advocate for your child in the school system. Parents are often left feeling alone at the table during an IEP meeting. We help change that.</p>
         <div class="mt-0 lg:mt-6 max-w-7xl sm:flex">
           <div class="mt-3 rounded-lg sm:mt-0">
             <button class="items-center block px-10 py-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-xl hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 bg-gn">Contact Us</button>
