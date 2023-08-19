35663df5c777168876028bd5222ac21d84d06b59
Mack Grissom <mcintyregrissom@gmail.com>
2023-08-19 11:12:53 +0700

    removed personal info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 
 //  <div id='contact'>
 //     <div className="relative bg-beige " >
-      
+
 //       <div className="absolute inset-0">
 //         <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 bg-beige" />
 //       </div>
@@ -97,7 +97,7 @@ import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 //                   placeholder="Email"
 //                 />
 //               </div>
-             
+
 //               <div>
 //                 <label htmlFor="message" className="sr-only">
 //                   Message
@@ -117,7 +117,7 @@ import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/24/outline'
 //                   value='Send'
 //                   className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-6 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 bg-gn "
 //                 />
-                  
+
 //               </div>
 //             </form>
 //           </div>
@@ -152,14 +152,14 @@ const Contact = () => {
   return (
 
     <>
-    <div classname='bg'>
-    <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl text-gn">Get in touch</h2>
-      <div className='flex justify-between mb-2'>
-            <p className="mt-3 ml-2 text-lg leading-6 text-gray-500 mb-3 align-left">
-             If you'd like to book a free 30 minutes consultation, gain access to critical resources or have any general questions - please reach out below. We look forward to hearing from you!
-             </p>
+      <div classname='bg'>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl text-gn">Get in touch</h2>
+        <div className='flex justify-between mb-2'>
+          <p className="mt-3 ml-2 text-lg leading-6 text-gray-500 mb-3 align-left">
+            If you'd like to book a free 30 minutes consultation, gain access to critical resources or have any general questions - please reach out below. We look forward to hearing from you!
+          </p>
 
-             {/* <dl className="mt-1 text-base text-gray-500">
+          {/* <dl className="mt-1 text-base text-gray-500">
                <div className="mt-6">
                  <dt className="sr-only">Phone number</dt>
                  <dd className="flex">
@@ -175,56 +175,56 @@ const Contact = () => {
                  </dd>
                </div>
              </dl> */}
-             </div>
-            
-    <form onSubmit={handleSubmit} className="bg-beige p-6 rounded-lg shadow-md">
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="user_name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
+        </div>
+
+        <form onSubmit={handleSubmit} className="bg-beige p-6 rounded-lg shadow-md">
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="user_name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="user_email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block text-gray-700 font-bold mb-2">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              rows="4"
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-end">
+            <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-gn" value='send'>
+              Send
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="user_email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="message" className="block text-gray-700 font-bold mb-2">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          rows="4"
-        ></textarea>
-      </div>
-      <div className="flex items-center justify-end">
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-gn" value='send'>
-          Send
-        </button>
-      </div>
-    </form>
-    </div>
-    <p className="mt-6 mb-5 text-xs text-gray-500">
-               Need A Professional Website?{' '}
-               <a href="https://www.mackgrissom.io" className="font-medium text-gray-700 underline">
-                 Contact Our Developer
-               </a>
-               
-             </p>
+      <p className="mt-6 mb-5 text-xs text-gray-500">
+        Need A Professional Website?{' '}
+        <a href="https://www.mackgrissom.io" className="font-medium text-gray-700 underline">
+          Contact Our Developer
+        </a>
+
+      </p>
     </>
   );
 };
